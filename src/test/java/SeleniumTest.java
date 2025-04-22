21cdee33b4b25e36c99a3ba2d4d691c191bbfc3f
waseem1234567891 <wasimtuff@gmail.com>
2025-04-22 18:02:24 +0200

    as

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -13,53 +11,26 @@ public class SeleniumTest {
     WebDriver driver;
     String baseUrl = "https://www.google.com";
 
-    @Parameters("browser")
     @BeforeMethod
-    public void setUp(String browser) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless", "--disable-gpu");
-            driver = new ChromeDriver(options);
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
-            driver = new FirefoxDriver(options);
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-    }
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
 
-    @Test
-    public void testGoogleHomePage() {
-        driver.get(baseUrl);
-        System.out.println("Title is: " + driver.getTitle());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
     }
 
     @Test
     public void testGoogleHomePageTitle() {
         driver.get(baseUrl);
         String title = driver.getTitle();
-        System.out.println("Title: " + title);
-        Assert.assertTrue(title.contains("Google"), "Title should contain 'Google'");
-    }
-
-    @Test
-    public void testGoogleSearchBoxPresence() {
-        driver.get(baseUrl);
-        WebElement searchBox = driver.findElement(By.name("q"));
-        Assert.assertTrue(searchBox.isDisplayed(), "Search box should be visible");
-    }
-
-    @Test
-    public void testGoogleSearchFunctionality() {
-        driver.get(baseUrl);
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium Grid");
-        searchBox.submit();
-
-        WebElement results = driver.findElement(By.id("search"));
-        Assert.assertTrue(results.isDisplayed(), "Search results should be visible");
-        Assert.assertTrue(driver.getTitle().toLowerCase().contains("selenium grid"));
+        System.out.println("Page title: " + title);
+        Assert.assertTrue(title.contains("Google"));
     }
 
     @AfterMethod
